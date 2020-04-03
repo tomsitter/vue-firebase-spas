99e4975630f4553cc62127054f78eacd5341df13
Thomas Sitter <thomas.sitter@gmail.com>
2020-04-02 21:11:59 -0700

    Set up navigation to pages

--- src/main.js
+++ src/main.js
@@ -1,10 +1,23 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+import {
+    faTrash, 
+    faLink, 
+    faListUl, 
+    faRandom, 
+    faStar, 
+    faEnvelope,
+    faUndo
+} from "@fortawesome/free-solid-svg-icons";
+
+library.add(faTrash, faLink, faListUl, faRandom, faStar, faEnvelope, faUndo);
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
-  render: h => h(App),
-}).$mount('#app');
+  render: h => h(App)
+}).$mount("#app");
---
+++ src/views/Meetings.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>This is a meetings page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Meetings',
+};
+</script>
